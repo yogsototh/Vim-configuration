b8dcf579541fae1a4a8b14d98a24204deffb859f
Yann Esposito <yann.esposito@gmail.com>
2012-08-03 12:05:49 +0200

    Added Shakespearian higlight

--- after/syntax/haskell.vim
+++ after/syntax/haskell.vim
@@ -125,3 +125,53 @@ hi link hsNiceOperator Operator
 hi! link Conceal Operator
 setlocal conceallevel=2
 
+" Syntax addition for Shakespearian templates
+
+let old_syntax = b:current_syntax
+unlet b:current_syntax
+
+syn include @hamlet syntax/hamlet.vim
+unlet b:current_syntax
+
+syn include @cassius syntax/cassius.vim
+unlet b:current_syntax
+
+syn include @lucius syntax/lucius.vim
+unlet b:current_syntax
+
+syn include @julius syntax/julius.vim
+unlet b:current_syntax
+
+
+syn region hmBlock   matchgroup=quasiQuote start=/\[\$\?[iws]\?hamlet|/ end=/|\]/ contains=@hamlet
+syn region hmBlock   matchgroup=quasiQuote start=/\[\$\?xs\?hamlet|/    end=/|\]/ contains=@hamlet
+syn region csBlock   matchgroup=quasiQuote start=/\[\$\?cassius|/       end=/|\]/ contains=@cassius
+syn region lcBlock   matchgroup=quasiQuote start=/\[\$\?lucius|/        end=/|\]/ contains=@lucius
+syn region jsBlock   matchgroup=quasiQuote start=/\[\$\?julius|/        end=/|\]/ contains=@julius
+
+" simple text
+syn region txtBlock  matchgroup=quasiQuote start=/\[\$\?[sl]t|/ end=/|\]/ contains=txtInterp
+syn region txtInterp matchgroup=txtInterpDelim start="#{"  end="}" contains=txtOp,txtString,txtNum
+
+syn match txtOp      contained /\(\$\|\.\)/
+syn match txtString  contained /"[^"]*"/
+syn match txtNum     contained /\<[0-9]*\>/
+
+if version < 508
+  command! -nargs=+ HiLink hi link <args>
+else
+  command! -nargs=+ HiLink hi def link <args>
+endif
+
+HiLink quasiQuote     Boolean
+HiLink txtBlock       String
+HiLink txtInterp      Normal
+HiLink txtInterpDelim Delimiter
+HiLink txtOp          Operator
+HiLink txtString      String
+HiLink txtNum         Number
+
+delcommand HiLink
+
+" restore current syntax value
+let b:current_syntax = old_syntax
---
+++ test/test.hs
@@ -0,0 +1,113 @@
+--
+-- Haskell source file
+--
+
+[hamlet|
+    <head>
+        <title>#{title}
+        <script src="/jquery.min.js">
+        <script src="/jquery-ui.min.js">
+
+    <!-- comment -->
+
+    $# comment
+
+    <body>
+        <p>Go to 
+        <a href="@{RoutR}">
+
+        <p .something #{what}>
+
+        <div id="accordion">
+            $forall tagGroup <- tagGroups
+                ^{addTagGroup tagGroup}
+
+
+        <script type="text/javascript">
+            $(function() {
+                $("#accordion").accordion({
+                    collapsible: true,
+                    autoHeight:  false,
+                    active:      false
+                });
+            });
+
+        <p>_{FooterMsg}
+    |]
+
+[hamlet|<a href=@{RouteR} title="foo">#{what}?|]
+
+[whamlet|
+    <head>
+        <h1>Foo
+
+    ^{stuff}
+|]
+
+[ihamlet|
+    <head>
+        <h1>Foo
+
+    _{i18nstuffs $ msg}
+|]
+
+[shamlet|
+    <p>
+        Some #{interpolations}
+    |]
+
+[xhamlet|
+    <xml>
+        <node>a #{a}
+        <node>b #{b}
+        <node>c @{CrouteR}
+    |]
+
+[xshamlet|
+    <xml>
+        <node>a #{a}
+        <node>b #{b}
+    |]
+
+[cassius|
+    body
+        /* comment */
+        color: #ffffff
+        font-size: 100%
+
+    .class
+        background-color:
+
+    .#{classAsVariable}
+        background-color: #ffffff
+    |]
+
+[lucius|
+    body {
+        /* comment */
+        color: #ffffff;
+        font-size: 100%;
+    }
+
+    .class {
+        background-color:
+    }
+
+    .#{classAsVariable} {
+        background-color: #ffffff;
+    }
+    |]
+
+[julius|
+    // comment
+    function f(_arg) {
+        var g = _arg + #{f . g $ h};
+        alert(g);
+    }
+
+    f(1.0);
+    |]
+
+[st|Some #{interpolated $ text}|]
+
+[lt|Some #{lazily $ interpolated $ text}|]
